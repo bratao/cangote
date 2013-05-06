c956fd82cd2d1aa74fd66c560e0d399e1133cd22
bratao <bratao@0ece9b63-09fb-49fa-bef2-506c893f8797>
2013-05-06 21:27:09 +0000

    Wrong place
    git-svn-id: http://svn.cangote.org@10 0ece9b63-09fb-49fa-bef2-506c893f8797

--- bugtracker/views.py
+++
@@ -1,9 +0,0 @@
-# Create your views here.
-from reqtool.bugtracker.admin import IssueAdmin
-from reqtool.bugtracker.models import Issue
-from django.contrib.admin.sites import AdminSite
-from django.contrib import admin
-
-def add_issue(request, queryset):
-    issue_admin = IssueAdmin(Issue, AdminSite())
-    return issue_admin.add_view(request)
